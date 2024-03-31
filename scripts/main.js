5253f0e0b12b29b4d3ed2021eb63d03475705ae0
yadhumanikandan <yadhumanikandan0@gmail.com>
2024-04-01 00:56:58 +0530

    basic

--- scripts/main.js
+++ scripts/main.js
@@ -23,8 +23,6 @@ app.addEventListener("click", function(event){
 async function open_terminal(){
   createText("Welcome");
   await delay(700);
-  createText("Starting the server...");
-  await delay(1500);
   createText("You can run several commands:");
  
   createCode("about me", "Who am i and what do i do.");
@@ -42,9 +40,9 @@ function new_line(){
   const span1 = document.createElement("span");
   const span2 = document.createElement("span");
   p.setAttribute("class", "path")
-  p.textContent = "# user";
-  span1.textContent = " in";
-  span2.textContent = " ~/heber-leonard";
+  p.textContent = "#user";
+  span1.textContent = "@";
+  span2.textContent = "~/yadhukrishnan";
   p.appendChild(span1);
   p.appendChild(span2);
   app.appendChild(p);
@@ -79,22 +77,22 @@ async function getInputValue(){
   }
   else if(value === "projects"){
     trueValue(value);
-    createText("<a href='https://github.com/heberleonard2' target='_blank'><i class='fab fa-github white'></i> github.com/heberleonard2</a>")
+    createText("<a href='https://github.com/yadhumanikandan' target='_blank'><i class='fab fa-github white'></i> github.com/yadhumanikandan</a>")
   }
   else if(value === "about me"){
     trueValue(value);
-    createText("Oi, meu nome é Héber ;)")
-    createText("Desenvolvedor atualmente focado em todo o ecossistema Javascript. Utilizando principalmente a stack <span class='blue'>Node, React e React Native </span>por permitir criar aplicações de forma descomplicada e produtiva.")
+    createText("Hi, My name is Yadhukrishnan ;)")
+    createText("I am a dedicated Cybersecurity Professional specializing in Penetration Testing, Bug Bounty Hunting, and Security Analysis. Committed to fortifying digital ecosystems and safeguarding critical assets. Let's collaborate to enhance resilience and ensure a secure digital future. Connect with me at <span class='blue'>yadhumanikandan0@gmail.com </span>to discuss cutting-edge cybersecurity strategies and fortifications.")
   }
   else if(value === "social -a"){
     trueValue(value);
-    createText("<a href='https://github.com/heberleonard2' target='_blank'><i class='fab fa-github white'></i> github.com/heberleonard2</a>")
-    createText("<a href='https://www.linkedin.com/in/heber-leonard/' target='_blank'><i class='fab fa-linkedin-in white'></i> linkedin.com/in/heber-leonard</a>")
-    createText("<a href='https://www.instagram.com/heber_leonard/' target='_blank'><i class='fab fa-instagram white'></i> instagram.com/heber_leonard</a>")
+    createText("<a href='https://github.com/yadhumanikandan' target='_blank'><i class='fab fa-github white'></i> github.com/yadhumanikandan</a>")
+    createText("<a href='https://www.linkedin.com/in/yadhukrishnan-m-m-09117a224/' target='_blank'><i class='fab fa-linkedin-in white'></i> linkedin.com/in/yadhukrishnan-m-m</a>")
+    createText("<a href='https://twitter.com/yadhumanikandan' target='_blank'><i class='fab fa-twitter white'></i> twitter.com/yadhumanikandan</a>")
   }
   else if(value === "social"){
     trueValue(value);
-    createText("Didn't you mean: social -a?")
+    createText("Didn't you mean: <span class=blue>social -a</span>?")
   }
   
   else if(value === "clear"){
@@ -148,7 +146,7 @@ function createCode(code, text){
   const p = document.createElement("p");
   p.setAttribute("class", "code");
   p.innerHTML =
- `${code} <br/><span class='text'> ${text} </span>`;
+ `${code} <span class='text'>- ${text} </span>`;
   app.appendChild(p);
 }
 
